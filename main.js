de92ede6894f9f8ba236476e56d8f67a04eee864
Sauma Saha <SaumaSaha@users.noreply.github.com>
2023-06-11 21:14:08 +0530

    Restructured the directory structure

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const { InsertModeController } = require("./insert-mode-controller");
-const { InputController } = require("./key-board-controller");
-const { Buffer, renderer } = require("./buffer");
+const { InsertModeController } = require("./src/insert-mode-controller");
+const { InputController } = require("./src/key-board-controller");
+const { Buffer, renderer } = require("./src/buffer");
 const fs = require("fs");
 
 
---
+++ test/buffer-test.js
@@ -0,0 +1,11 @@
+const { describe, it } = require("node:test");
+const { deepStrictEqual, strictEqual } = require("assert");
+const { Buffer } = require("../src/buffer");
+
+describe("Buffer", () => {
+  describe("storeText", () => {
+    it("should store the text in the buffer", () => {
+
+    });
+  });
+});
